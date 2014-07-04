dc5ee582c1b4dfadc746b186162e56ba27e01845
Chris Dent <chdent@redhat.com>
2014-07-04 15:23:48 +0100

    First mighty spike at a notification plugin
    This is very much a work in progress, so much so that there's not
    much point even committing it for review yet, using github as my
    location indepdendent storage.
    
    This borrows heavily from the compute notifications code. The basic
    idea is that there are a few classes of samples that can be had
    (Fan, Temperature, Voltage, Current). Within these are potentially
    multiple individual sensors and values. In all cases the volume of
    a sample is always in 'Sensor Reading'.
    
    Each class has a _transform_id and _transform_reading method which
    can be used to bring the data in line with expected forms.
    
    Questions:
    
    * How much of the rest of the payload do we want to save, and thus
      give to the sample constructor?
    * What is an event_type and how do we figure it out?
    * Do we need to insure that numeric values are saved as numbers, not
      string representations thereof?
    * Are the sample.nameS that are being created the correct way to
      deal with samples coming from specific devices? Or does that info
      go somewhere else? If a single instance of hardware has ten temp
      readings, how do we distinguish which reading?

---
+++ ceilometer/hardware/notifications/ipmi.py
@@ -0,0 +1,86 @@
+#
+# Copyright 2014 Red Hat
+#
+# Author: Chris Dent <chdent@redhat.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Converters for producing hardware sensor data sample messages from
+notification events.
+"""
+
+from ceilometer import plugin
+from ceilometer.openstack.common.gettextutils import _
+from ceilometer.openstack.common import log
+from ceilometer.openstack.common import timeutils
+from ceilometer import sample
+
+LOG = log.getLogger(__name__)
+
+class SensorNotification(plugin.NotificationBase):
+
+    event_types = ['hardware.*']
+    metric = None
+
+    @staticmethod
+    def get_targets(conf):
+        return [] #  Nothing for now
+
+    def _get_sample(self, message):
+        try:
+            return (payload for _, payload
+                    in message['payload'][self.metric].items())
+        except KeyError:
+            return []
+
+    def _transform_id(self, data):
+        return data
+
+    def _transform_reading(self, data):
+        return data
+
+    def _package_payload(self, message, payload):
+        info = {}
+        info['publisher_id'] = message['publisher_id']
+        info['resource_id'] = message['instance_uuid']
+        info['timestamp'] = str(timeutils.parse_strtime(
+            message['timestamp'], '%Y%m%d%H%M%S'))
+        info['event_type'] = 'I DO NOT KNOW'
+        info['payload'] = payload
+        return info
+
+    def process_notification(self, message):
+        payloads = self._get_sample(message)
+        for payload in payloads:
+            info = self._package_payload(message, payload)
+            yield sample.Sample.from_notification(
+                name='hardware.%s.%s' % (self.metric.lower(),
+                                         self._transform_id(
+                                             info['payload']['Sensor ID'])),
+                type=self.sample_type,
+                unit=self.unit,
+                volume=self._transform_reading(info['payload']['Sensor Reading']),
+                user_id=None,
+                project_id=None,
+                resource_id=info['resource_id'],
+                message=info)
+
+class TemperatureSensorNotification(SensorNotification):
+    metric = 'Temperature'
+    sample_type = sample.TYPE_GAUGE
+    unit = 'C'
+
+    def _transform_id(self, data):
+        return data.lower().replace(' ', '_')
+
+    def _transform_reading(self, data):
+        return data.split(' ', 1)[0]
